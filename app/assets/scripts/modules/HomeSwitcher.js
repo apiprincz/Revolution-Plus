ab1d7920bc1eca4083fb6b1d8bbc7c007d87fe61
Niyi <princzny@gmail.com>
2020-06-18 12:04:19 +0100

    about page update

--- app/assets/scripts/modules/HomeSwitcher.js
+++ app/assets/scripts/modules/HomeSwitcher.js
@@ -3,25 +3,48 @@ class HomeImageSwitcher {
 
     constructor() {
 
-        this.homeImage = document.querySelectorAll(".home__box");
-        this.navHandler();
-        
+        this.home = document.querySelectorAll('.home__box')
+        this.pagination = document.querySelectorAll('.home__pagination > div')
+        this.counter = 0
+
+        this.navHandler()
+        this.showImage()
 
     }
 
 
     navHandler() {
-
-       
-        for (let x = 0; x < this.homeImage.length; x++) {
-        
-            Console.log('news')
+        setInterval(() => {
+            for (let x = 0; x < this.home.length; x++) {
+                this.home[x].classList.remove('home__show')
+                this.pagination[x].classList.remove('home__pagination-bg')
+                
+    
+                for (let i = 0; i < this.home.length; i++) {
+                    this.home[this.counter].classList.add('home__show')
+                    this.pagination[this.counter].classList.add('home__pagination-bg')
+                    
+                }
+                
+            }
+    if (this.counter === 3) {
+        this.counter = 0
+    } else {
+    this.counter+=1
     }
+            
+        }, 
+        4000);
+       
+   
 
    
 
 
 }
+
+  
+
 }
 
 
